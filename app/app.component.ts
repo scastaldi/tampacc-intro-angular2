b715479f3217d9ddd619db2dad67e8f530e8c506
Sebastian <sebastian@admins-MacBook.lan>
2016-07-04 10:25:24 -0400

    minor updates

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: '<div>Hello World!</div>',
+    template: '<div>Hello Tampa Code Camp!</div>',
     directives: [] 
 })
 export class AppComponent {}
